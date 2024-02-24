f908bc0ba46b3fb2513f3dd6e0452bf3432da4d1
gustavo Alves da Silva <gustavo.twister10@gmail.com>
2024-02-24 18:35:59 -0300

    Add files via upload

---
+++ assets/js/poke-api.js
@@ -0,0 +1,62 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    pokemon.height = pokeDetail.height;
+    pokemon.weight = pokeDetail.weight;
+    pokemon.species = pokeDetail.species.name;
+
+    const abilities = pokeDetail.abilities.map((abilities) => {
+        return fetch(abilities.ability.url)
+        .then((response) => response.json())
+        .then((abilityData) => {
+            return {
+                name: abilityData.name,
+                description: abilityData.flavor_text_entries[0].flavor_text
+            };
+        });
+    });
+    return Promise.all(abilities)
+    .then((abilitiess) => {
+        pokemon.abilities = abilities
+        return pokemon
+    })    
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
+    return fetch(url)
+        .then((response) => response.json()) 
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        
+}
+
+pokeApi.getAbilities = (name) => {
+    const url = `https://pokeapi.co/api/v2/ability/${name}/`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => console.log(jsonBody))
+}
